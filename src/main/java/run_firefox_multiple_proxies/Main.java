875fb08b62805d0c2136eeb17ccaba27843b84fa
dustytrash <Lionelbergen@live.com>
2022-08-17 22:23:59 -0500

    added spotless, setup first variable check

--- src/main/java/run_firefox_multiple_proxies/Main.java
+++ src/main/java/run_firefox_multiple_proxies/Main.java
@@ -1,7 +1,12 @@
 package run_firefox_multiple_proxies;
 
 public class Main {
-	public static void main(String[] args) {
-		String appDataFolder = System.getenv("APPDATA");
-	}
+  public static void main(String[] args) {
+    String appDataFolder = System.getenv("APPDATA");
+
+    if (appDataFolder == null) {
+      throw new RuntimeException(
+          "FATAL ERROR, env variable APPDATA does not exist. Its used to get FireFox roaming folder location.");
+    }
+  }
 }
